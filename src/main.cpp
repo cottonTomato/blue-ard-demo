c096188620b29a0fada6a9b18415326613b80b7d
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-04-03 12:59:24 +0530

    created client

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,50 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 
-#define DEVICE_NAME "ESP32-Abhi"
+#define CLIENT_NAME "ESP32-Adi"
+#define SERVER_NAME "ESP32-Abhi"
+
+#define NUM_LEDS 4
+#define LED_STATE_SIZE (NUM_LEDS * sizeof(bool))
+
+uint8_t ledPins[NUM_LEDS] = {32, 33, 34, 35};
 
 BluetoothSerial SerialBT;
 
 void setup()
 {
   Serial.begin(115200);
+
+  Serial.println("Setting up LED Pins");
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
+    uint8_t pin = ledPins[i];
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
+
   Serial.println("Starting Classic BT");
-  SerialBT.begin(DEVICE_NAME, true);
+  SerialBT.begin(CLIENT_NAME, false);
   Serial.println("Bluetooth Setup Complete");
 
-  // SerialBT.register_callback([](esp_spp_cb_event_t event, esp_spp_cb_param_t *param) {
-  //   switch (event)
-  //   {
-  //   case ESP_SPP_SRV_OPEN_EVT:
-  //     Serial.println("Client Connected");
-  //     break;
-    
-  //   default:
-  //     break;
-  //   }
-  // });
+  Serial.println("Attaching Callback on Data Recieve");
+  SerialBT.onData([](const uint8_t *buffer, size_t size)
+                  {
+    if (size != LED_STATE_SIZE) {
+      return;
+    }
+    for (int i = 0; i < NUM_LEDS; i++) {
+      Serial.print(buffer[i] ? "1" : "0");
+      uint8_t pin = ledPins[i];
+      digitalWrite(pin, buffer[i] ? HIGH : LOW);
+    } });
 }
 
 void loop()
 {
-  if (SerialBT.hasClient())
+  if (!SerialBT.hasClient())
   {
-    if (SerialBT.available())
-    {
-      Serial.write(SerialBT.read());
-    }
-    if (Serial.available())
-    {
-      SerialBT.write(Serial.read());
-    }
-  } else {
-    SerialBT.connect("ESP32-Adi");
+    SerialBT.connect(SERVER_NAME);
   }
-  delay(100);
+  delay(1000);
 }
